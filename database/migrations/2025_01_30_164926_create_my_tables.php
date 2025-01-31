ca92a4b67008ced6fa3bf10867447a7a3a0f94bb
MartinKulic <kulich@stud.uniza.sk>
2025-01-31 16:05:59 +0100

    my redo part1

---
+++ database/migrations/2025_01_30_164926_create_my_tables.php
@@ -0,0 +1,76 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+//use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+//use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('path');
+            $table->string('name');
+            $table->text('desc');
+            $table->text('caption');
+            $table->unsignedBigInteger('autor_id');
+            $table->timestamp('s_date')->useCurrent();
+            $table->foreign('autor_id')->references('id')->on('users');
+        });
+
+        Schema::create("tags", function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+        });
+
+        Schema::create('image_tag', function (Blueprint $table) {
+            $table->unsignedBigInteger('tag_id');
+            $table->unsignedBigInteger('image_id');
+
+            $table->primary(['tag_id', 'image_id']);
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+            $table->foreign('image_id')->references('id')->on('images')->onDelete('cascade');
+        });
+
+        Schema::create('rating', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('image_id');
+            $table->tinyInteger('value');
+            $table->timestamp('r_date')->useCurrent();
+
+            $table->primary(['user_id', 'image_id']);
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('image_id')->references('id')->on('images')->onDelete('cascade');
+        });
+
+        Schema::create('favs', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('image_id');
+            $table->timestamp('f_date')->useCurrent();
+
+            $table->primary(['user_id', 'image_id']);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('image_id')->references('id')->on('images')->onDelete('cascade'); //Mozno by nemusel byt cascade
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ratings');
+        Schema::dropIfExists('favs');
+        Schema::dropIfExists('image_tags');
+        Schema::dropIfExists('tags');
+        Schema::dropIfExists('images');
+    }
+};
